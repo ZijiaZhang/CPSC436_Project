b566830a6666cc91e500fe912ab99079f5ccd18f
Zijia Zhang <Zijia.Zhang99@gmail.com>
2020-05-23 17:17:29 -0700

    Add react example with typescript

---
+++ src/frontend/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {CBUTTON} from './Button';
+
+const App = () => {   //this is how you make a functional component
+  return (
+      <div><CBUTTON /></div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './frontend/components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './frontend/reducers';  //we exported combineReducers
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+        </Provider>,
+document.getElementById('root')
+);
+
