85f93e4a17aa4daa9006873d040f63c4af509ec1
Zijia Zhang <Zijia.Zhang99@gmail.com>
2020-06-09 18:55:48 -0700

    Add support for router

--- src/App.ts
+++ src/App.ts
@@ -5,5 +5,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 console.log(path.join(__dirname, 'public'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
