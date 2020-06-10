69a9f62dfc45e007f7b264770ac23be00dcffa45
m1771 <y.zhao.16@alumni.ubc.ca>
2020-06-09 18:38:54 -0700

    Modified Router - pinned the separated shared components into AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,22 +5,31 @@ import {
     Switch
 } from "react-router-dom";
 
-import App from "./frontend/share/components/App";
 import PostPage from "./frontend/posts_page/components/App";
 import ChatRoom from "./frontend/chat_room/components/App";
 import Settings from "./frontend/settings/components/App";
+import Sidebar from "./frontend/share/components/Sidebar";
+import FriendList from "./frontend/share/components/FriendList";
 
 class AppRouter extends Component {
     render() {
         return (
             <Router>
                 <div>
-                    <App />
-                    <Switch>
-                        <Route exact path="/" component={Settings} />
-                        <Route exact path="/postsPage" component={PostPage} />
-                        <Route exact path="/chatRoom" component={ChatRoom} />
-                    </Switch>
+                    <Sidebar  user_info={ {image_path: './images/test2.png', name:'Dennis'}}/>
+                    <div className="central-panel">
+                        <Switch>
+                            <Route path="/" component={Settings} />
+                            <Route path="/postsPage" component={PostPage} />
+                            <Route path="/chatRoom" component={ChatRoom} />
+                        </Switch>
+                    </div>
+                    <FriendList  friends={[
+                        {image_path: './images/dora.png', name:'Will'},
+                        {image_path: './images/test.png', name:'Gary'},
+                        {image_path: './images/test2.png', name:'Dennis'},
+                        {image_path: './images/1.ico', name:'Rommel'}
+                    ]}/>
                 </div>
             </Router>
         );
