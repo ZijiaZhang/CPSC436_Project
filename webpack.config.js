c5bcb42d30f87dc535267a85f84badbced5bf4a4
y0w1b <55099033+y0w1b@users.noreply.github.com>
2020-06-09 18:41:45 -0700

    Added react router, also changed the name of a class in postsPage (#11)
    * Add react-router
    
    * react-router Routes the four components (temporary), will be modified when frontend is more complete
    
    * CHANGELOG modified, which was forgotten in the previous commit.
    
    * Modified Router
    
    * Modified Router - pinned the separated shared components into AppRouter
    
    Co-authored-by: m1771 <y.zhao.16@alumni.ubc.ca>

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ module.exports = {
     mode: "development",
     devtool: "source-map",
     entry: {
-        main: './src/index.tsx',
-        chat_room: './src/frontend/chat_room/index.tsx',
-        settings: './src/frontend/settings/index.tsx',
-        posts_page: './src/frontend/posts_page/index.tsx'
+        main: './src/index.tsx'
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
