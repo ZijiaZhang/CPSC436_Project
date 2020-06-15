ece0b90194300384205b4be084a6008589512a54
Ningyuan Xu <xu.ningyuan@outlook.com>
2020-06-15 00:46:19 -0700

    router does not work

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -20,29 +20,32 @@ class AppRouter extends Component {
             <Switch>
                 <Route exact path="/" component={LoginPage}/>
                 <Route path="/registerPage" component={RegisterPage}/>
-                
-                <div className={'row'}>
-                    <Sidebar  user_info={ {image_path: './images/test2.png', name:'Denise'}}/>
-                    <div className="central-panel">
-                        <Switch>
-                            <Route path="/postPage" component={PostPage} />
-                            <Route path="/settings" component={Settings} />
-                            <Route path="/chatRoom" component={ChatRoom} />
-                            <Route path="/searchPage" component={SearchPage} />
-                        </Switch>
-                    </div>
-                    <FriendList  friends={[
-                        {image_path: './images/dora.png', name:'Will'},
-                        {image_path: './images/test.png', name:'Gary'},
-                        {image_path: './images/test2.png', name:'Denise'},
-                        {image_path: './images/1.ico', name:'Rommel'}
-                    ]}/>
-                </div>
-
-
+                <Route path="/home" component={Home}/>
             </Switch>
         );
     }
+}
+
+const Home = () => {
+    return (
+        <div className={'row'}>
+        <Sidebar  user_info={ {image_path: './images/test2.png', name:'Denise'}}/>
+        <div className="central-panel">
+            <Switch>
+                <Route exact path="/" component={PostPage} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/chatRoom" component={ChatRoom} />
+                <Route path="/searchPage" component={SearchPage} />
+            </Switch>
+        </div>
+        <FriendList  friends={[
+            {image_path: './images/dora.png', name:'Will'},
+            {image_path: './images/test.png', name:'Gary'},
+            {image_path: './images/test2.png', name:'Denise'},
+            {image_path: './images/1.ico', name:'Rommel'}
+        ]}/>
+    </div>
+    );
 }
 
 export default AppRouter;
