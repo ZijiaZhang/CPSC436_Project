6c23b5be45c34c100be52b86da8a4a01da28d789
m1771 <y.zhao.16@alumni.ubc.ca>
2020-06-15 21:01:22 -0700

    Pull from master and start to finalize posts page

---
+++ src/App.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+console.log(path.join(__dirname, 'public'));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
