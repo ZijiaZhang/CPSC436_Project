d08368c384f67667a5ca0058e21f7d79f028334f
m1771 <y.zhao.16@alumni.ubc.ca>
2020-06-18 09:42:46 -0700

    Modified posts page with more components, reducers, actions and updated css style.

---
+++ src/frontend/posts_page/components/ArticlePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ArticlePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+export default ArticlePage
--- src/frontend/posts_page/components/PostsLayout.tsx
+++ src/frontend/posts_page/components/PostsLayout.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import PostBlock, {Post} from './PostBlock';
-
+import PostBlock, {Post, PostBlockProps} from './PostBlock';
+import {connect} from "react-redux";
 
 interface PostsLayoutProps {
-  posts: Post[];
+  postList: Post[];
 }
 
 class PostsLayout extends React.Component<PostsLayoutProps, {}> {
+  constructor(props: PostsLayoutProps) {
+    super(props);
+  }
 
   render() {
-    const listPosts = this.props.posts.map((post) =>
+    const postList: Post[] = this.props.postList.slice().reverse();
+    const listPosts = postList.map((post) =>
       <PostBlock post={post} />
     );
     return (
@@ -18,7 +22,10 @@ class PostsLayout extends React.Component<PostsLayoutProps, {}> {
       </div>
     );
   }
-
 }
 
-export default PostsLayout;
+const mapStateToProps = (state: { postList: Post[]; }) => { //name is by convention
+  return { postList: state.postList }; //now it will appear as props
+};
+
+export default connect(mapStateToProps)(PostsLayout);
