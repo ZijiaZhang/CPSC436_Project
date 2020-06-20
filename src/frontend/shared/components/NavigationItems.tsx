fe57863cc6e011d0b9baca141db816c929fd6014
Ningyuan Xu <xu.ningyuan@outlook.com>
2020-06-20 16:23:15 -0700

    add_friend_item_to_menu_bar

--- src/frontend/shared/components/NavigationItems.tsx
+++ src/frontend/shared/components/NavigationItems.tsx
@@ -8,7 +8,8 @@ class NavigationItems extends React.Component<IUserProps,{}> {
         <div>
             <a><img src={this.props.avatarPath} alt="img not found" width="100" height="100"/><p>{this.props.name}</p></a>
             <Link to="/" className="glyphicon glyphicon-home">Home</Link>
-            <Link to="/chatRoom" className="glyphicon glyphicon-user">Chats</Link>
+            <Link to="/searchPage" className="glyphicon glyphicon-user">Friends</Link>
+            <Link to="/chatRoom" className="glyphicon glyphicon-envelope">Chats</Link>
             <Link to="/settings" className="glyphicon glyphicon-cog">Setting</Link>
         </div>
         );
