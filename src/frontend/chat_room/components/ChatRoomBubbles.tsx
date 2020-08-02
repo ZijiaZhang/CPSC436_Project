d9549af61628f789c33a0535e446cb19012a88b6
m1771 <y.zhao.16@alumni.ubc.ca>
2020-08-01 20:51:35 -0700

    Fixed Chat Room Avatar

--- src/frontend/chat_room/components/ChatRoomBubbles.tsx
+++ src/frontend/chat_room/components/ChatRoomBubbles.tsx
@@ -17,7 +17,8 @@ export class ChatRoomBubbles extends React.Component<ISingleMessage, {}> {
     render() {
         return <div className={'conversation-bubble-wrap'}>
             <div className={['received', 'sent', 'unsent'][this.props.status]}>
-                <img className={'conversation-avatar-' + ['received', 'sent', 'unsent'][this.props.status]} src={this.props.sender.avatarPath} alt={this.props.sender.fullname}/>
+                <img className={'conversation-avatar-' + ['received', 'sent', 'unsent'][this.props.status]}
+                     src={this.props.sender.avatarPath ? this.props.sender.avatarPath : './images/photoP.png'} alt={this.props.sender.fullname}/>
                 <p className = {'conversation-bubble ' + ['received', 'sent', 'unsent'][this.props.status]} >{this.props.message}</p>
             </div>
         </div>;
